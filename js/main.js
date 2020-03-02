e16e9249b63892e2b143ff5e5b99b3d785fca346
asmfadholi <=>
2020-03-02 23:45:05 +0700

    give another try

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker
-            .register('../service-worker-try/sw_cached_pages.js')
+            .register('/service-worker-try/sw_cached_pages.js')
             .then(reg => console.log('Service Worker: Registered (Pages)'))
             .catch(err => console.log(`Service Worker: Error: ${err}`));
     });
